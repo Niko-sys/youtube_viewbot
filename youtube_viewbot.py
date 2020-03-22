7191a51b0a4624d21b0c456c0fb239fe457b27fe
cuijian <32232191+cuijian0819@users.noreply.github.com>
2020-03-22 20:14:42 +0900

    add parser

--- youtube_viewbot.py
+++ youtube_viewbot.py
@@ -1,12 +1,21 @@
 import webbrowser
 import time
 import os
+import optparse
+
+wait_time = 3 # time to wait until video is laoded and played
+
+parser = optparse.OptionParser()
+parser.add_option('-v', dest='view', type='int', default=3)
+parser.add_option('-t', dest='time', type='int', default=10)
+(options, args) = parser.parse_args()
+#print(options.view, options.time)
 
 controller = webbrowser.get()
 
-for i in range(5):
+for i in range(options.view):
 	controller.open("https://www.youtube.com/watch?v=yOpF54plrM0")
-	time.sleep(3)
+	time.sleep(wait_time)
 
-time.sleep(300)
+time.sleep(options.time)
 os.system("killall -9 'Google Chrome'")
